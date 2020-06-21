8c3c960224109fce8334f5614249b0ab34395829
Dongdong Yu <dongdongyu@MacBook-Pro.local>
2020-06-22 05:49:10 +1000

    update

--- server.js
+++ server.js
@@ -12,12 +12,12 @@ app.use(express.static("files"));
 
 app.use("/api", routes);
 
-if (process.env.NODE_ENV === "production") {
-	app.use(express.static(path.join(__dirname, "build")));
-	app.get(/^\/(?!api).*/, (req, res) => {
-		res.sendFile(path.resolve(__dirname, "build", "index.html"));
-	});
-}
+// if (process.env.NODE_ENV === "production") {
+app.use(express.static(path.join(__dirname, "build")));
+app.get(/^\/(?!api).*/, (req, res) => {
+	res.sendFile(path.resolve(__dirname, "build", "index.html"));
+});
+// }
 
 app.listen(process.env.PORT || 3001, () => {
 	console.log("Listening on port 3001");
